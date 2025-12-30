4d6c0307c33bbdbed5a40b175aed172174f4a073
shubhAgrawal666 <shubhagrawal666@gmail.com>
2025-12-30 18:25:40 +0530

    added vercel.json in client

--- client/src/api/admin.js
+++ client/src/api/admin.js
@@ -1,20 +1,15 @@
-
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "/api",
   withCredentials: true,
 });
 
-export const getDashboardStats = () =>
-  API.get("/admin/dashboard-stats");
+export const getDashboardStats = () => API.get("/admin/dashboard-stats");
 
+export const getAllUsers = () => API.get("/admin/users");
 
-export const getAllUsers = () =>
-  API.get("/admin/users");
-
-export const getUserById = (id) =>
-  API.get(`/admin/user/${id}`);
+export const getUserById = (id) => API.get(`/admin/user/${id}`);
 
 export const updateUserRole = (payload) =>
   API.put("/admin/update-role", payload);
@@ -22,32 +17,22 @@ export const updateUserRole = (payload) =>
 export const toggleUserVerification = (payload) =>
   API.put("/admin/toggle-verification", payload);
 
-export const deleteUser = (id) =>
-  API.delete(`/admin/delete-user/${id}`);
+export const deleteUser = (id) => API.delete(`/admin/delete-user/${id}`);
 
-export const createCourse = (payload) =>
-  API.post("/courses/create", payload);
-
+export const createCourse = (payload) => API.post("/courses/create", payload);
 
 export const updateCourse = (slug, payload) =>
   API.put(`/courses/update/${slug}`, payload);
 
+export const deleteCourse = (slug) => API.delete(`/courses/delete/${slug}`);
 
-export const deleteCourse = (slug) =>
-  API.delete(`/courses/delete/${slug}`);
-
-
-export const getAdminCourses = () =>
-  API.get("/courses");
-
+export const getAdminCourses = () => API.get("/courses");
 
 export const addLesson = (slug, payload) =>
   API.post(`/courses/${slug}/lessons/add`, payload);
 
-
 export const updateLesson = (slug, lessonSlug, payload) =>
   API.put(`/courses/${slug}/lessons/${lessonSlug}`, payload);
-
 
 export const deleteLesson = (slug, lessonSlug) =>
   API.delete(`/courses/${slug}/lessons/${lessonSlug}`);
